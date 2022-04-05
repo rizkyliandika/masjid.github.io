8e4155de6c292035b4a4add9caf85eb7cae9e4b5
dryrez <drreznetz@gmail.com>
2022-04-05 21:07:09 +0700

    Update DKM > PELAYAN MASJID

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -3,9 +3,9 @@ export const Team = (props) => {
     <div id='team' className='text-center'>
       <div className='container'>
         <div className='col-md-8 col-md-offset-2 section-title'>
-          <h2>DKM</h2>
+          <h2>PELAYAN MASJID</h2>
           <p>
-            Dewan Kesejahteraan Masjid
+            Pelayan Masjid Fatimah Muhammad
           </p>
         </div>
         <div id='row'>
